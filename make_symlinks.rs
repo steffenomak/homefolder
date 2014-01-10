983ee995bf5e5a2a4fe79ead500b96d48f58a08a
Stefan Risberg <steffenomak@gmail.com>
2014-01-10 15:25:48 +0100

    Updated to latest rust.

--- make_symlinks.rs
+++ make_symlinks.rs
@@ -12,31 +12,19 @@ fn parse_file(file: File) -> ~[Entry] {
 
     let mut buff_read = BufferedReader::new(file);
 
-    while !buff_read.eof() {
-        let s = match buff_read.read_line() {
-            Some(s) => s,
-            None => break,
-        };
-
+    for l in buff_read.lines() {
         let mut str_list: ~[~str] = ~[];
 
-        for part in s.words() {
+        for part in l.words() {
             str_list.push(part.into_owned());
         }
 
-        if str_list.len() == 0 {
-            break;
-        }
-
-        match Entry::new_from_array(str_list) {
-            Some(e) => entries.push(e),
-            None => {
-                println(format!("Error, nr in str_list: {:u}", str_list.len()));
-
-                for i in str_list.iter() {
-                    println(*i);
-                }
-            },
+        if str_list.len() != 0 {
+            match Entry::new_from_array(str_list) {
+                Some(e) => entries.push(e),
+                None => println(format!("Error, nr in str_list: {:u}", 
+                                        str_list.len()))
+            }
         }
     }
 
